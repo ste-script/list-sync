27ae78e58aad29c4f9e413f96abd6bd6609444ef
Stefano Babini <stefano.babini5@studio.unibo.it>
2025-01-22 17:11:55 +0100

    test consumer

--- python/kafka-project/src/consumer.py
+++ python/kafka-project/src/consumer.py
@@ -1,18 +1,25 @@
+# filepath: /home/ste/Documents/list-sync/python/kafka-project/src/consumer.py
 from confluent_kafka import Consumer, KafkaException, KafkaError
-import uuid
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 conf = {
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': uuid.uuid1(),
+    # Ensure this matches the advertised listener
+    'bootstrap.servers': 'broker:9092',
+    'group.id': 'my_consumer_group',
     'auto.offset.reset': 'earliest',
-    'security.protocol': 'PLAINTEXT',
 }
+
 consumer = Consumer(conf)
 
 
 def consume_messages():
     try:
         consumer.subscribe(['wal'])
+        logger.info("Subscribed to topic 'wal'")
 
         while True:
             msg = consumer.poll(timeout=1.0)
@@ -20,15 +27,16 @@ def consume_messages():
                 continue
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
-                    print(f"End of partition reached {msg.partition()}")
-                elif msg.error():
+                    logger.info(f"End of partition reached {msg.partition()}")
+                else:
+                    logger.error(f"Error: {msg.error()}")
                     raise KafkaException(msg.error())
             else:
-                print(f"Received message: {msg.value().decode(
+                logger.info(f"Received message: {msg.value().decode(
                     'utf-8')} from {msg.topic()} [{msg.partition()}]")
 
     except KafkaException as e:
-        print(f"Failed to consume messages: {e}")
+        logger.error(f"Failed to consume messages: {e}")
     finally:
         consumer.close()
 
